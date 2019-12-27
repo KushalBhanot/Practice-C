208d146e5103c1ce050bed633bb2fc09061337a6
Kushal Bhanot <developerkushalb@gmail.com>
2019-12-27 17:17:49 +0530

    Easy problem

---
+++ 1. Playing With Characters.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch,s[50],s1[90];
+
+    scanf("%c",&ch);
+    printf("%c\n",ch);
+
+    scanf("%s",&s);
+    printf("%s\n",s);
+
+    gets(s1);
+    prints(s1);
+
+    return 0;
+}
+
